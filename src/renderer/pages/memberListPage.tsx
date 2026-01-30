2c2362b112763a2aa2e229006ea3ecd88b6f3b82
amami-harhid <amami-thida100@outlook.jp>
2026-01-30 19:24:05 +0900

    0.0.2 (3)

---
+++ src/renderer/pages/memberListPage.tsx
@@ -0,0 +1,13 @@
+import { useRef, useState } from "react";
+export function MemberListPage() {
+    return (
+        <>
+        <h1 className="pageTitle"><span>利用者一覧</span></h1>
+        <div className="modal" >
+            <div className="modal-content">
+                <div className="card"><p ></p></div>
+            </div>
+        </div>
+        </>
+    );
+}
