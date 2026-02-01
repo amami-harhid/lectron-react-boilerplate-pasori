631ace93c8c0f8a13e146ee964052ed3e7716829
amami-harhid <amami-thida100@outlook.jp>
2026-02-01 22:24:32 +0900

    0.0.4 (7) 履歴ページ整える

--- src/renderer/pages/historiesPage.tsx
+++ src/renderer/pages/historiesPage.tsx
@@ -1,11 +1,9 @@
-import { ChangeEvent, useRef, useState, useMemo } from "react";
+import { useRef, useState } from "react";
 import * as Histories from '../../db/histories/histories';
-import * as Cards from '../../db/cards/cards';
-import { CardRow } from '../../db/cards/cardRow';
 import { HistoriesCardRow } from '../../db/histories/historiesRow';
 import * as DateUtils from '../../utils/dateUtils';
 
-type ROW = {
+type TABLE_ROW = {
     id:number,
     fcno:string,
     name:string,
@@ -15,31 +13,29 @@ type ROW = {
 }
 type PAGEINFO = {
     date: string,
-    tableData: ROW[],
+    tableData: TABLE_ROW[],
 }
+const CHANNEL_SQL_REQUEST = 'asynchronous-sql-command';
+const CHANNEL_SQL_REPLY = 'asynchronous-sql-reply';
 export function HistoriesPage() {
     const [pageInfo, setPageInfo] = useState<PAGEINFO>({date:'', tableData:[]});
-    const [input_date, setInput_date] = useState<string>('');
-    const [data, setData] = useState<ROW[]>([]);
-    const histories_reload = useRef<HTMLButtonElement>(null);
     const histories_dateResult = useRef<HTMLSpanElement>(null);
-    const historiesTbody = useRef<HTMLTableSectionElement>(null);
 
     const select = async (date:Date): Promise<HistoriesCardRow[]> => {
         console.log('select executed');
         window.electron.ipcRenderer.sendMessage(
-            'asynchronous-sql-command',
+            CHANNEL_SQL_REQUEST,
             Histories.hist_selectByDate.name, date);
-        const rows: HistoriesCardRow[] 
-            = await window.electron.ipcRenderer.asyncOnce<HistoriesCardRow[]>('asynchronous-sql-reply');    
+        const rows: HistoriesCardRow[]
+            = await window.electron.ipcRenderer.asyncOnce<HistoriesCardRow[]>(CHANNEL_SQL_REPLY);
         return rows;
     }
-    const historiesView = async (date:Date):Promise<ROW[]> => {
+    const historiesToTableData = async (date:Date):Promise<TABLE_ROW[]> => {
         const rows:HistoriesCardRow[] = await select(date);
-        const _data:ROW[] = [];
+        const _data:TABLE_ROW[] = [];
         for(const row of rows){
             const newId = _data.length > 0 ? _data[_data.length - 1].id + 1 : 1;
-            const newRow:ROW = {
+            const newRow:TABLE_ROW = {
                 id: newId,
                 fcno: row.fcno,
                 name: (row.name)?row.name:'',
@@ -51,40 +47,39 @@ export function HistoriesPage() {
         }
         return _data;
     }
-    
+
     const handleInputChange = async (event: React.ChangeEvent<HTMLInputElement>)=>{
-        console.log('tableChange executed!');
-        const own = event.target
-        const date = own.valueAsDate;
-        const rows:ROW[] = []
+        const date = event.target.valueAsDate;
         if(date){
-            if(histories_dateResult && histories_dateResult.current){
-                const date_str = DateUtils.dateToSqlite3Date(date);
-                histories_dateResult.current.textContent = date_str;
-                rows.push(...await historiesView(date));
-                setPageInfo( {date:date_str, tableData:rows} )
-            }
+            const _rows = await historiesToTableData(date);
+            const date_str = DateUtils.dateToSqlite3Date(date);
+            // ここでレンダリングされる（テーブルへのデータ反映）
+            pageRender(date_str, _rows);
         }else{
-            dateInit();
+            // ここでレンダリングされる（テーブル初期化）
+            pageRender();
         }
 
     }
-    const dateInit = () => {
-        setPageInfo( {date:'', tableData:[]} )
+    const pageRender = (_data:string='', _tableData:TABLE_ROW[]=[]) => {
+        setPageInfo( {date:_data, tableData:_tableData} )
     }
-    console.log('addEventLister change')
-    //setPageCounter(pageCounter+1);
-//    histories_inputDate.current?.removeEventListener('change', tableChange);
-//    histories_inputDate.current?.addEventListener('change', tableChange);
+    const pageInit = () => {
+      // ページレンダリングされる
+      pageRender();
+    }
+
     return (
         <div className ="mainPanel">
             <div className="modal_histories">
                 <div className="modal-content">
-                    <label htmlFor="histories_inputDate">日付を選択してください:</label>
-                    <input type="date" value={pageInfo.date} onChange={handleInputChange}/>&nbsp;<button onClick={dateInit}>初期化</button>
-                    <p>選択された日付: <span ref={histories_dateResult}></span></p>
+                    <label>日付を選択してください:
+                        <input type="date" value={pageInfo.date} onChange={handleInputChange}/>
+                        &nbsp;<button onClick={pageInit}>初期化</button>
+                    </label>
+                    <p>選択された日付: <span>{pageInfo.date}</span></p>
                 </div>
-                <table id="histories_table" className="appTable">
+                <table className="appTable">
                   <tbody>
                     <tr>
                         <th>NO</th>
@@ -95,7 +90,7 @@ export function HistoriesPage() {
                         <th>退室</th>
                     </tr>
                 </tbody>
-                <tbody ref={historiesTbody}>
+                <tbody>
                     {pageInfo.tableData.map(row=>(
                        <tr key={row.id}>
                             <td>{row.id}</td>
@@ -104,9 +99,9 @@ export function HistoriesPage() {
                             <td>{row.kana}</td>
                             <td>{row.in}</td>
                             <td>{row.out}</td>
-                       </tr> 
+                       </tr>
                     ))}
-                </tbody> 
+                </tbody>
                 </table>
             </div>
         </div>
